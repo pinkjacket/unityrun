2bd6d4fcfc79c4f359fc5ad5b871d38ecba57c1c
James Donlan <jacketlizard@live.com>
2019-07-21 01:27:28 -0700

    generate a long line of connected individual platforms

---
+++ Assets/Platforms/GenerateWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    public GameObject platform;
+    void Start()
+    {
+        Vector3 pos = new Vector3(0, 0, 0);
+        for (int i = 0; i < 20; i++)
+        {
+            Instantiate(platform, pos, Quaternion.identity);
+            pos.z += 10;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
