9841ca70475b323d48beed9aa370b9d6f7c641cf
James Donlan <jacketlizard@live.com>
2019-07-24 01:38:39 -0700

    minor fix to prevent stairs from sometimes deleting themselves early

--- Assets/Scripts/Deactivate.cs
+++ Assets/Scripts/Deactivate.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Deactivate : MonoBehaviour
 {
+    bool dScheduled = false;
    void OnCollisionExit(Collision player)
     {
-        if (player.gameObject.tag == "Player")
+        if (player.gameObject.tag == "Player" && !dScheduled)
+        {
             //platform disappears after 3 seconds
             Invoke("SetInactive", 3.0f);
+            dScheduled = true;
+        }
     }
     void SetInactive()
     {
         this.gameObject.SetActive(false);
+        dScheduled = false;
     }
 }
