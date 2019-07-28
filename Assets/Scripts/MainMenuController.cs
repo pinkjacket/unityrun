228da12c646bc8177d292278d11afdce7a180917
James Donlan <jacketlizard@live.com>
2019-07-28 15:08:12 -0700

    menu buttons now pop up their respective panels, which can be closed

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject helpPanel;
+    GameObject[] panels;
+    GameObject[] mmButtons;
 
     void Start()
     {
-        helpPanel.SetActive(false);
+        //creating arrays of the menu buttons and panels they open
+        panels = GameObject.FindGameObjectsWithTag("subpanel");
+        mmButtons = GameObject.FindGameObjectsWithTag("mmbutton");
+
+        //hiding panels
+        foreach (GameObject p in panels)
+            p.SetActive(false);
     }
 
-    public void CloseHelpPanel()
+    public void ClosePanel(Button button)
     {
-        helpPanel.SetActive(false);
+        //reaches into the button's parent panel to close it
+        button.gameObject.transform.parent.gameObject.SetActive(false);
+        foreach (GameObject b in mmButtons)
+            b.SetActive(true);
     }
 
-    public void OpenHelpPanel()
+    public void OpenPanel(Button button)
     {
-        helpPanel.SetActive(true);
+        button.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        //hide buttons when a panel opens
+        foreach (GameObject b in mmButtons)
+            if (b != button.gameObject)
+                b.SetActive(false);
     }
 
     public void LoadGameScene()
