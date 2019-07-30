08097921d1d2f2b92330ba83b26ebac8e7431fd0
James Donlan <jacketlizard@live.com>
2019-07-29 21:11:20 -0700

    coins can now be picked up, increasing player score

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+    public static GameData singleton;
+    public Text scoreText = null;
+    public int score = 0;
+
+    void Awake()
+    {
+        GameObject[] gd = GameObject.FindGameObjectsWithTag("gamedata");
+
+        if(gd.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        singleton = this;
+    }
+
+    public void UpdateScore(int s)
+    {
+        score += s;
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
+    }
+
+}
